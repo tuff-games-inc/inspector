2468a87721b54f6078fb2f281ffbd21e4fa97dc5
Freddie Yershon <github@freddieyershon.co.uk>
2026-08-24 21:43:06 +0100

    timer variables

--- scripts/gui_handler.gd
+++ scripts/gui_handler.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var day: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,4 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
+	print("timer up")
+	day += 1
+	print(day)	
 	pass # Replace with function body.
