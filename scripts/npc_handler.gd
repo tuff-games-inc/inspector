7e4d975733c45b1127245cf040eff5a9df76ae73
Freddie Yershon <github@freddieyershon.co.uk>
2026-08-26 17:19:38 +0100

    add random entity

--- scripts/npc_handler.gd
+++ scripts/npc_handler.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
 @onready var anim_player = $"../Camera3D/AnimationPlayer"
-@export var entity01 = load("res://entities/entity01.tscn")
+@export var entity01:PackedScene  = load("res://entities/entity01.tscn")
+@export var entity02:PackedScene = load("res://entities/entity02.tscn")
 @onready var gui_handler = $"../Camera3D/GUI"
 var isSpawned = false
 var isMoving = false
 var npc: CharacterBody3D = null
+var possible_entities: Array[PackedScene] = [entity01, entity02]
+
 
 signal change_score(direction: bool)
 
@@ -25,12 +28,13 @@ func _spawnHandler() -> void:
 	if !npc:
 		isSpawned = true
 		isMoving = true
-		entity01_spawn()
+		entity_spawn()
 	else:
 		print("npc already spawned")
 
-func entity01_spawn() -> void:
-	npc = entity01.instantiate() as CharacterBody3D
+func entity_spawn() -> void:
+	var random_entity: PackedScene = possible_entities.pick_random()	
+	npc = random_entity.instantiate() as CharacterBody3D
 	add_child(npc)
 	anim_player.play("npc_move_to_counter")
 	await anim_player.animation_finished
@@ -60,6 +64,7 @@ func _replyHandler(outcome: bool) -> void:
 			await anim_player.animation_finished
 			change_score.emit(false)
 		npc.queue_free()
+		npc = null
 		isSpawned = false
 	else:
 		print("moving")
